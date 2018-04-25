5894ede53742a6044aea159ec06b45b03f1d4c11
danidz96 <dani.d.zamorano@gmail.com>
2018-04-25 20:19:00 +0200

    Configurar Paypal

--- config.php
+++ config.php
@@ -2,4 +2,12 @@
 
 require 'paypal/autoload.php';
 
+$apiContext = new \PayPal\Rest\ApiContext(
+  new \PayPal\Auth\OAuthTokenCredential(
+    'AebP0y8pW7zkkntmQa_6LtVcPnmUVTNgdBNdJjU9nTMw5IaMQflDtSeYgXflkbYcFb2yW6CNm3SGzxvX',//ID Cliente
+    'ECdem7uOcPEhpUGrMBv2CuhQpTU2ANk25uC9Gh1c6cfoX1cryNVgZHi8FujRz_IXRJKv0VZfFnd91H5T'//Secret
+    )
+  );
+
+
  ?>
